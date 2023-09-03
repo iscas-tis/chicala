1634150e347b4c81270d3bfe611e1d1e68970656
liuyic00 <liuyic00@gmail.com>
2023-09-03 20:50:54 +0800

    test(case): substitution some param

--- testcase/src/main/scala/example/rocketchip/Mul.scala
+++ testcase/src/main/scala/example/rocketchip/Mul.scala
@@ -35,33 +35,36 @@ class Mul(
     mulEarlyOut: Boolean = false,
     divEarlyOut: Boolean = false,
     divEarlyOutGranularity: Int = 1,
-    width: Int,
+    w: Int,
     nXpr: Int = 32
 ) extends Module {
   // require(mulUnroll > 0)
   private def minMulLatency = if (mulEarlyOut) 2 else w / mulUnroll
   def minLatency: Int       = minMulLatency
 
-  val io = IO(new MultiplierIO(width, log2Up(nXpr)))
+  val io = IO(new MultiplierIO(w, log2Up(nXpr)))
 
-  val w        = io.req.bits.in1.getWidth
   val mulw     = if (mulUnroll == 0) w else (w + mulUnroll - 1) / mulUnroll * mulUnroll
-  val fastMulW = if (mulUnroll == 0) false else w / 2 > mulUnroll && w % (2 * mulUnroll) == 0
+  val fastMulW = if (mulUnroll == 0) false else w / 2 > mulUnroll && w % (2 * mulUnroll) == 0 // 5
 
   val s_ready :: s_mul :: s_done_mul :: Nil = Enum(3)
 
   val state = RegInit(s_ready)
 
-  val req       = Reg(new MultiplierReq(width, log2Up(nXpr)))
-  val count     = Reg(UInt(log2Ceil(mulw / mulUnroll).W))
-  val neg_out   = Reg(Bool())
-  val isHi      = Reg(Bool())
-  val resHi     = Reg(Bool())
-  val divisor   = Reg(UInt((w + 1).W))        // div only needs w bits
-  val remainder = Reg(UInt((2 * mulw + 2).W)) // div only needs 2*w+1 bits // 15
+  val req = Reg(new MultiplierReq(w, log2Up(nXpr)))
+  val count = Reg(
+    UInt(log2Ceil((if (mulUnroll == 0) w else (w + mulUnroll - 1) / mulUnroll * mulUnroll) / mulUnroll).W)
+  )
+  val neg_out = Reg(Bool())          // 10
+  val isHi    = Reg(Bool())
+  val resHi   = Reg(Bool())
+  val divisor = Reg(UInt((w + 1).W)) // div only needs w bits
+  val remainder = Reg(
+    UInt((2 * (if (mulUnroll == 0) w else (w + mulUnroll - 1) / mulUnroll * mulUnroll) + 2).W)
+  ) // div only needs 2*w+1 bits
 
-  val cmdMul, cmdHi, lhsSigned, rhsSigned = WireInit(false.B) // 16 - 19
-  if (mulUnroll != 0) { // 20
+  val cmdMul, cmdHi, lhsSigned, rhsSigned = WireInit(false.B) // 15 - 18
+  if (mulUnroll != 0) { // 19
     switch(io.req.bits.fn) {
       is(0.U) { // aluFn.FN_MUL
         cmdMul    := true.B  // 20.1.1.1.1
@@ -73,9 +76,9 @@ class Mul(
   }
 
   // require(w == 32 || w == 64)
-  def halfWidth(reqDw: UInt) = (w > 32).B && reqDw === false.B // 21
+  def halfWidth(reqDw: UInt) = (w > 32).B && reqDw === false.B // 20
 
-  def sext(x: UInt, halfW: Bool, signed: Bool) = { // 22
+  def sext(x: UInt, halfW: Bool, signed: Bool) = { // 21
     val sign = signed && Mux(halfW, x(w / 2 - 1), x(w - 1))
     val hi   = Mux(halfW, Fill(w / 2, sign), x(w - 1, w / 2))
     (Cat(hi, x(w / 2 - 1, 0)), sign)
@@ -83,23 +86,23 @@ class Mul(
   val (lhs_in, lhs_sign) = sext(io.req.bits.in1, halfWidth(io.req.bits.dw), lhsSigned)
   val (rhs_in, rhs_sign) = sext(io.req.bits.in2, halfWidth(io.req.bits.dw), rhsSigned)
 
-  val subtractor        = remainder(2 * w, w) - divisor // 25
-  val result            = Mux(resHi, remainder(2 * w, w + 1), remainder(w - 1, 0))
+  val subtractor        = remainder(2 * w, w) - divisor
+  val result            = Mux(resHi, remainder(2 * w, w + 1), remainder(w - 1, 0)) // 25
   val negated_remainder = -result
 
   val mulReg         = Cat(remainder(2 * mulw + 1, w + 1), remainder(w - 1, 0))
   val mplierSign     = remainder(w)
-  val mplier         = mulReg(mulw - 1, 0)                           // 30
-  val accum          = mulReg(2 * mulw, mulw).asSInt
+  val mplier         = mulReg(mulw - 1, 0)
+  val accum          = mulReg(2 * mulw, mulw).asSInt // 30
   val mpcand         = divisor.asSInt
   val prod           = Cat(mplierSign, mplier(mulUnroll - 1, 0)).asSInt * mpcand + accum
   val nextMulReg     = Cat(prod, mplier(mulw - 1, mulUnroll))
-  val nextMplierSign = count === (mulw / mulUnroll - 2).U && neg_out // 35
+  val nextMplierSign = count === (mulw / mulUnroll - 2).U && neg_out
 
   val eOutMask = (
     (BigInt(-1) << mulw).S >>
       (count * mulUnroll.U)(log2Up(mulw) - 1, 0)
-  )(mulw - 1, 0)
+  )(mulw - 1, 0) // 35
   val eOut = (mulEarlyOut).B &&
     count =/= (mulw / mulUnroll - 1).U &&
     count =/= 0.U &&
@@ -112,7 +115,7 @@ class Mul(
     Mux(eOut, eOutRes, nextMulReg)(mulw - 1, 0)
   )
 
-  if (mulUnroll != 0) when(state === s_mul) { // 40
+  if (mulUnroll != 0) when(state === s_mul) { // 39
     remainder := Cat(nextMulReg1 >> w, nextMplierSign, nextMulReg1(w - 1, 0))
 
     count := count + 1.U
@@ -121,10 +124,10 @@ class Mul(
       resHi := isHi
     }
   }
-  when(io.resp.ready && io.resp.valid) { // 41
+  when(io.resp.ready && io.resp.valid) { // 40
     state := s_ready
   }
-  when(io.req.ready && io.req.valid) { // 42
+  when(io.req.ready && io.req.valid) { // 41
     state     := Mux(cmdMul, s_mul, s_ready)
     isHi      := cmdHi
     resHi     := false.B
@@ -137,10 +140,10 @@ class Mul(
 
   val outMul = (state & (s_done_mul)) === (s_done_mul)
   val loOut  = Mux(fastMulW.B && halfWidth(req.dw) && outMul, result(w - 1, w / 2), result(w / 2 - 1, 0))
-  val hiOut  = Mux(halfWidth(req.dw), Fill(w / 2, loOut(w / 2 - 1)), result(w - 1, w / 2)) // 45
-  io.resp.bits.tag := req.tag
+  val hiOut  = Mux(halfWidth(req.dw), Fill(w / 2, loOut(w / 2 - 1)), result(w - 1, w / 2))
+  io.resp.bits.tag := req.tag // 45
 
   io.resp.bits.data := Cat(hiOut, loOut)
   io.resp.valid     := (state === s_done_mul)
-  io.req.ready      := state === s_ready // 49
+  io.req.ready      := state === s_ready // 48
 }
